26842e2db4c5c94ca20e8c307da06e5f3b13c7b2
Joel Oguntoye <joeloguntoye@gmail.com>
2021-03-10 16:51:04 +0100

    Setting up firebase authentication dependencies

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+c
+3app/src/main/java/com/app/myinvoice/MainActivity.kt,0\f\0f479c33efa6ac3ce3a5e459980914573f643ee8
---
+++ app/src/main/java/com/app/presentation/authentication/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.app.presentation.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.myinvoice.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
